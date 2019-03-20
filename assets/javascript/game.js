0daee2a4381d118c95c6924a69d06058c1cbe901
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-03-20 13:50:46 -0500

    added to my java script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,51 @@
 window.onload = function () {
-// my variables
-            var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l',
-                            'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-            var wins = 0;
-            var losses = 0;
-            var guessesleft = 10;
-            var userGuess = event.key;
-            var guesses = [ ];
+    // my variables
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    var wins = 0;
+    var losses = 0;
+    var guessesleft = 10;
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    var guesses = [];
 
-            var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    function randomletter() {
+        return alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
 
-            console.log(computerChoice)
-            console.log(userGuess)
+    document.onkeypress = function (event) {
+        var userGuess = event.key;
+        console.log('user guess: ', userGuess);
+        console.log('computer choice', computerChoice);
+        if (userGuess === computerChoice) {
+            wins++;
+            guessesleft = 10;
+            guesses = [];
+            console.log('win!');
+            computerChoice = randomletter();
+        } else {
+            guessesleft--;
+        }
+
+        if (guessesleft === 0) {
+            losses++;
+            guessesleft = 10;
+            guesses = [];
+            computerChoice = randomletter();
+        }
+
+        if (guesses.indexOf(userGuess) >= 0) {
+            guesses = [];
+        } else {
+            guesses.push(userGuess);
+            document.getElementById('guesses').innerHTML = guesses
+        }
+
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        document.getElementById('losses').innerHTML = "losses: " + losses;
+        document.getElementById('guessesleft').innerHTML = "Guesses left: " + guessesleft;
+        document.getElementById('guesses').innerHTML = "Guesses: " + guesses;
+
+    }
 
 
-                    function randomletter() {
-                        computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-                    }
-                
-                
-                
-                            document.onkeypress = function(event) {
-                                var userGuess = event.key;
-                                
-
-                                if(userGuess === computerChoice){
-                                    wins++;
-                                    guessesleft = 10;
-                                    guesses = [ ];
-                                }else{
-                                    guessesleft--;
-                                }
-                            
-                                if(guessesleft === 0){
-                                    losses++;
-                                    guessesleft = 10;
-                                    guesses = [ ];
-                                    
-                                }
-
-                                if(guesses.indexOf(userGuess) >=0){
-                                    guesses = [ ];
-                                } else{
-                                    guesses.push(userGuess);
-                                    document.getElementById('guesses').innerHTML = guesses
-                                }
-                            
-                                document.getElementById('wins').innerHTML = "Wins: " + wins;
-                                document.getElementById('losses').innerHTML = "losses: " + losses;
-                                document.getElementById('guessesleft').innerHTML = "Guesses left: " + guessesleft;
-                                document.getElementById('guesses').innerHTML = "Guesses: " + guesses;
-                            
-                            
-
-                    }   
-                    
 }
